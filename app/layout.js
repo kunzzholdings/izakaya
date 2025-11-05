9a5c8eba8e4450b717c70495a56ae23629ab4ba2
dale <darrellchua118@gmail.com>
2025-11-05 15:15:01 +0800

    9

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata = {
   title: 'Tokyo Izakaya Cuisine',
   description: 'Tokyo Izakaya Cuisine是一家专注于日本料理的餐厅，我们提供最正宗的日本料理， 等美味。',
+  icons: {
+    icon: '/favicon.ico',
+  },
 }
 
 export default function RootLayout({ children }) {
