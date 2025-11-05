c17d14a96d0852f0a5000064d182ec2286ee7615
dale <darrellchua118@gmail.com>
2025-11-05 15:11:35 +0800

    8

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Tokyo Japanese Cuisine',
-  description: 'Tokyo Japanese Cuisine是一家专注于日本料理的餐厅，我们提供最正宗的日本料理， 等美味。',
+  title: 'Tokyo Izakaya Cuisine',
+  description: 'Tokyo Izakaya Cuisine是一家专注于日本料理的餐厅，我们提供最正宗的日本料理， 等美味。',
 }
 
 export default function RootLayout({ children }) {
